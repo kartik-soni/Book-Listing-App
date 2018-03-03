f04c53f83b9fd692e14593817c71fa62b5df7f61
Kartik Soni <sonikartik98@outlook.com>
2018-03-04 00:34:29 +0530

    HTTP URL generated from input

--- app/src/main/java/com/example/kartik/booklistingapp/MainActivity.java
+++ app/src/main/java/com/example/kartik/booklistingapp/MainActivity.java
@@ -1,13 +1,50 @@
 package com.example.kartik.booklistingapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userInput;
+    ImageButton searchButton;
+    TextView noResultFound;
+    ListView listItem;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userInput = findViewById(R.id.edit_Text);
+        searchButton = findViewById(R.id.imageButton);
+        noResultFound = findViewById(R.id.no_Result_found);
+        listItem = findViewById(R.id.listView);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Convert the String URL into a URI object (to pass into the Intent constructor)
+                Uri earthquakeUri = Uri.parse(getUrlForHttpRequest());
+
+                // Create a new intent to view the earthquake URI
+                Intent websiteIntent = new Intent(Intent.ACTION_VIEW, earthquakeUri);
+
+                // Send the intent to launch a new activity
+                startActivity(websiteIntent);
+            }
+        });
+    }
+
+    private String getUrlForHttpRequest() {
+        final String Url = "https://www.googleapis.com/books/v1/volumes?q="+userInput.getText().toString();
+        return Url;
     }
 }
